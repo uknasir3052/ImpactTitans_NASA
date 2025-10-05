1e55fd35ec77deb81b16c6c0e98c3ea97d1ec0c6
Jahanvi11-1 <jpj.jariwala11@gmail.com>
2025-10-05 14:05:55 +0530

    App.tsx and Visualization3D.tsx

--- Impact-Titans/src/react-app/App.tsx
+++ Impact-Titans/src/react-app/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Landing from "@/react-app/pages/Landing";
+import Dashboard from "@/react-app/pages/Dashboard";
+import Visualization3D from "@/react-app/pages/Visualization3D";
+import Simulation from "@/react-app/pages/Simulation";
+import Game from "@/react-app/pages/Game";
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/3d-visualization" element={<Visualization3D />} />
+        <Route path="/simulation" element={<Simulation />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
